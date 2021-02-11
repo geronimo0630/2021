971d7d262ce5e6ede8b40935b6dc666f1d9c9720
geronimo0630 <gero_buitrago@hotmail.com>
2021-02-11 07:39:35 -0500

    subir clase 2

---
+++ clases/clase2.py
@@ -0,0 +1,30 @@
+prueba1 = True
+prueba2 = False
+print (prueba2)
+print (prueba1)
+prueba1 = prueba2
+print(prueba1)
+edad = 19
+estatura = 1.85
+peso = 78
+NOMBRE = "Geronimo buitrago"
+print ("#"*15,"mayorEdad","#"*15)
+isMayorEdad = edad >= 18
+print (isMayorEdad)
+print ("#"*15,"Bajo a la Estatura Promedio","#"*15)
+isMayorEstatura = estatura < 1.70 
+print (isMayorEstatura)
+print ("#"*15,"Peso diferente a 84","#"*15)
+isPesoDiferente = peso != 84 
+print (isPesoDiferente)
+apellido = "buitrago"
+isApellido = apellido in NOMBRE
+print ("#"*15,"Peso diferente a 84","#"*15)
+print (isApellido)
+
+
+
+
+
+
+
---
+++ clases/operaciones.py
@@ -0,0 +1,16 @@
+numeroA = 87
+numeroB = 83 
+sumar = numeroA + numeroB
+print ("el resultado es", sumar)
+restar = numeroA - numeroB
+print ("el resultado es ",restar)
+multiplicar = numeroA * numeroB
+print ("el resultado es",multiplicar)
+dividir = numeroA / numeroB
+print ("el resultado es",dividir)
+exponente = numeroA ** numeroB
+print ("el resultado es",exponente)
+
+
+
+
