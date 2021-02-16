a77ae2567f7d73f2207cd04e0d0e3baa1f21fac4
geronimo0630 <gero_buitrago@hotmail.com>
2021-02-16 08:09:09 -0500

    clase1

--- clases/clase1.py
+++ clases/clase1.py
@@ -3,6 +3,7 @@ saludo = "hola mundo mi pc"
 #se utilizo para saber la edad un int y para la estatura un float
 edad = 19
 estatura = 1.85
+peso = 78
 # al utilizar print hacemos que muestre en pantalla lo colocado anteriormente
-print(saludo, edad, estatura)
+print(saludo, edad, estatura,peso)
 
