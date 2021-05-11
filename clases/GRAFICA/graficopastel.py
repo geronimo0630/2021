fa3b8d3e595082bee1e7f35a0e56269983bf82bf
geronimo0630 <gero_buitrago@hotmail.com>
2021-05-11 08:41:06 -0500

    quiz 11 de marzo

--- clases/GRAFICA/graficopastel.py
+++ clases/GRAFICA/graficopastel.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
 pielabels =['mate', 'fisica ','programacion','humanidades']
 sizes = [30,25,15,10]
 explode = [0,0,0.1,0]
 plt.pie(sizes,labels = pielabels)
 ###########
-plt.title('materias mas perdidas por los estudiantes')
+plt.title('materiaimport matplotlib.pyplot as plt
+s mas perdidas por los estudiantes')
 plt.show()
